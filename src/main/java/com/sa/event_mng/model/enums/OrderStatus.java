d2f19ed41f610097b6e74797d8769984658f6330
canhhocit <canhhuu26@gmail.com>
2026-09-23 22:25:54 +0700

    fix: add COMPLETED value to OrderStatus enum

--- src/main/java/com/sa/event_mng/model/enums/OrderStatus.java
+++ src/main/java/com/sa/event_mng/model/enums/OrderStatus.java
@@ -3,5 +3,6 @@ package com.sa.event_mng.model.enums;
 public enum OrderStatus {
     PENDING, //chờ thanh toán hoặc xác nhận đơn hàng
     CONFIRMED, //đơn hàng dc xác nhận
-    CANCELLED //đơn bị hủy
+    CANCELLED, //đơn bị hủy
+    COMPLETED //đơn hàng hoàn thành
 }
