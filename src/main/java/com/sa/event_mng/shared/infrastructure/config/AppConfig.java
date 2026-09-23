e971f311062f934e7886ba9291df6502240b1439
canhhocit <canhhuu26@gmail.com>
2026-09-23 17:29:53 +0700

    perf: enable Spring Cache (@EnableCaching) for lightning fast in-memory category & event retrieval

--- src/main/java/com/sa/event_mng/shared/infrastructure/config/AppConfig.java
+++ src/main/java/com/sa/event_mng/shared/infrastructure/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.sa.event_mng.shared.infrastructure.config;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableAsync
 @EnableScheduling
+@EnableCaching
 public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
